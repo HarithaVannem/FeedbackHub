090c8005057d9f0b882b0f951c47e188358d6ca6
VannemreddiHaritha <47218056+VannemreddiHaritha@users.noreply.github.com>
2024-04-03 13:45:16 -0400

    Commit the new code

--- index.js
+++ index.js
@@ -23,5 +23,13 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 mongoose.connect(keys.mongoURI);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (res, req) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5003;
 app.listen(PORT);
