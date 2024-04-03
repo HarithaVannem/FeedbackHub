325e7bfd6f1ecb9af32dfb0ebdedfd3ee61644dd
VannemreddiHaritha <47218056+VannemreddiHaritha@users.noreply.github.com>
2024-04-03 14:03:19 -0400

    message

--- index.js
+++ index.js
@@ -27,9 +27,10 @@ mongoose.connect(keys.mongoURI);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require("path");
-  app.get("*", (res, req) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
 const PORT = process.env.PORT || 5003;
 app.listen(PORT);
